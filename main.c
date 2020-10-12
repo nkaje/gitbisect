ad3501d3198a6ec83c9405d21c09179c02d8254b
Naveen Kaje <naveen.kaje@juul.com>
2020-10-12 18:29:19 -0500

    main: make a different function

--- main.c
+++ main.c
@@ -7,12 +7,16 @@
 #define CONST_ONE 10
 #define CONST_TWO 0
 
+int do_operation(int a, int b)
+{
+    return (a/b);
+}
+
 int main()
 {
     int a = CONST_ONE;
     int b = CONST_TWO;
 
-
-    printf("a / b = %d\n", (a/b));
+    printf("a / b = %d\n", do_operation(a, b));
     return 0;
 }
