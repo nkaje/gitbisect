916e679b35b8960e209ac355e2281ddca34a92fe
Naveen Kaje <naveen.kaje@juul.com>
2020-10-12 18:23:20 -0500

    main: Add license and use consts

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
+/*
+ * Defalut License
+ */
+
 #include <stdio.h>
+
+#define CONST_ONE 10
+#define CONST_TWO 0
 
 int main()
 {
-    int a = 0;
-    int b = 0;
+    int a = CONST_ONE;
+    int b = CONST_TWO;
 
 
     printf("a / b = %d\n", (a/b));
     return 0;
 }
-
