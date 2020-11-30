1d8eb97005350631243c4838b096117fbc84381e
Naveen Kaje <naveen.kaje@juul.com>
2020-11-30 10:45:43 -0600

    combined commit
    use this to demo splitting changs with "git add - p"

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Sample README.md
+
+Sample text here
--- main.c
+++ main.c
@@ -12,11 +12,17 @@ int do_operation(int a, int b)
     return (a/b);
 }
 
+int do_another_operation(int a, int b)
+{
+    return (a*b);
+}
+
 int main()
 {
     int a = CONST_ONE;
     int b = CONST_TWO;
 
     printf("a / b = %d\n", do_operation(a, b));
+    printf("a * b = %d\n", do_another_operation(a, b));
     return 0;
 }
